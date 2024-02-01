2b149bafdff2740c2e95755de0a57a34d53a3a29
AlexV <sasichkamega@gmail.com>
2024-02-01 17:47:22 +0300

    up

--- core/src/BridgeImpl.hpp
+++ core/src/BridgeImpl.hpp
@@ -18,14 +18,10 @@ namespace dm_bridge {
     //thread safe for libuv thread
     void on_listen_tcp_handle_error(const uvw::error_event&, uvw::tcp_handle&) noexcept;
     void on_listen_tcp_handle_close(const uvw::close_event&, uvw::tcp_handle&) noexcept;
-    void on_listen_tcp_handle_end(const uvw::end_event&, uvw::tcp_handle&) noexcept;
     void on_listen_tcp_handle_listen(const uvw::listen_event&, uvw::tcp_handle&) noexcept;
     void on_listen_tcp_handle_shutdown(const uvw::shutdown_event&, uvw::tcp_handle&) noexcept;
 
 
-    void on_listen_tcp_handle_end(const uvw::end_event&, uvw::tcp_handle&) noexcept;
-    void on_listen_tcp_handle_end(const uvw::end_event&, uvw::tcp_handle&) noexcept;
-    void on_listen_tcp_handle_end(const uvw::end_event&, uvw::tcp_handle&) noexcept;
   };
 
 } // dm_bridge
